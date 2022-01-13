d5605bf97d94dc503919067a37f9b47bcd45ae03
samandar-dev <samandarbohodirov01@gmail.com>
2022-01-13 10:33:46 +0500

    start

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let elopenmodal = document. getElementById('openModal');
+let elColsemodal = document. getElementById('close-btn');
+let elModal = document. getElementById('Modal');
+
+
+elopenmodal.addEventListener('click', function(){
+  elModal.classList.add('show');
+});
+
+elColsemodal.addEventListener('click', function(){
+  elModal.classList.remove('show');
+});
+
